4afd96c6784fb62f94a2685496fb5477dc8507dd
Aline Frade <alinefrad@gmail.com>
2024-05-08 16:42:23 -0300

    routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
+import 'package:MobilEasy/configurations.dart';
 import 'package:MobilEasy/createAccount.dart';
 import 'package:MobilEasy/map.dart';
 import 'package:MobilEasy/menu.dart';
+import 'package:MobilEasy/profile.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,11 +17,14 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Onboarding Screen',
       home: MaterialYou(),
+      // home: LoginPage();
       debugShowCheckedModeBanner: false,
 
       routes: {
         'navigationPage': (context) => NavigationPage(),
         'createAccount': (context) => CreateProfile(),
+        'configurateProfile': (context) => Configurations(),
+        'profilePage': (context) => Profile(),
       },
     );
   }
