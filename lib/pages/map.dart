aca17f72cb8051777d23ffd4d460b3af16bbc4ca
Aline Frade <alinefrad@gmail.com>
2024-05-24 15:21:27 -0300

    markers

--- lib/pages/map.dart
+++ lib/pages/map.dart
@@ -13,8 +13,8 @@ class NavigationPage extends StatefulWidget {
 class _NavigationPageState extends State<NavigationPage> {
 
   late GoogleMapController _googleMapController;
-  // Marker _origin;
-  // Marker _destination;
+  Marker? _origin;
+  Marker? _destination;
 
   @override
   void dispose() {
@@ -33,16 +33,52 @@ class _NavigationPageState extends State<NavigationPage> {
       appBar: AppBar(
         backgroundColor: const Color.fromARGB(255, 0, 100, 0),
         title: const Text('Navegação'),
+        actions: [
+          TextButton(
+            onPressed: _origin == null
+                ? null
+                : () => _googleMapController.animateCamera(
+                      CameraUpdate.newCameraPosition(
+                        CameraPosition(
+                          target: _origin!.position,
+                          zoom: 14.5,
+                          tilt: 50.0,
+                        ),
+                      ),
+                    ),
+            child: const Text(
+              'Center Origin',
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+          TextButton(
+            onPressed: _destination == null
+                ? null
+                : () => _googleMapController.animateCamera(
+                      CameraUpdate.newCameraPosition(
+                        CameraPosition(
+                          target: _destination!.position,
+                          zoom: 14.5,
+                          tilt: 50.0,
+                        ),
+                      ),
+                    ),
+            child: const Text(
+              'Center Destination',
+              style: TextStyle(color: Colors.white),
+            ),
+          ),
+        ],
       ),
       body: GoogleMap(
         myLocationButtonEnabled: false,
         initialCameraPosition: _initialCameraPosition,
         onMapCreated: (controller) => _googleMapController = controller,
-        markers: const {
-          // if(_origin != null) _origin,
-          // if(_destination != null) _destination,
+        markers: {
+          if (_origin != null) _origin!,
+          if (_destination != null) _destination!,
         },
-        //onLongPress: _addMarker(),
+        onLongPress: _addMarker,
       ),
       floatingActionButton: FloatingActionButton(
         backgroundColor: Theme.of(context).primaryColor,
@@ -54,8 +90,27 @@ class _NavigationPageState extends State<NavigationPage> {
       ),
     );
   }
-  
-  _addMarker(LatLng pos) {
 
+  void _addMarker(LatLng pos) {
+    if (_origin == null || (_origin != null && _destination != null)) {
+      setState(() {
+        _origin = Marker(
+          markerId: const MarkerId('origin'),
+          infoWindow: const InfoWindow(title: 'Origin'),
+          icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueGreen),
+          position: pos,
+        );
+        _destination = null;
+      });
+    } else {
+      setState(() {
+        _destination = Marker(
+          markerId: const MarkerId('destination'),
+          infoWindow: const InfoWindow(title: 'Destination'),
+          icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueBlue),
+          position: pos,
+        );
+      });
+    }
   }
 }
